287df6fd9528c8a902afc47250359ff809bfb5bb
wlsdksql12 <90670621+wlsdksql12@users.noreply.github.com>
2026-06-15 16:27:17 +0900

    변수, 배열, 객체 개념 복습

---
+++ app.js
@@ -0,0 +1,31 @@
+const a = 5;
+const b = 2;
+let myName = "asj";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + myName);
+
+myName = "Seong Jin";
+
+console.log("your new name is " + myName);
+
+const amIFat = null;
+let something;
+console.log(something);
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+console.log(daysOfWeek);
+
+const player = {
+  name: "asj",
+  points: 10,
+  fat: true,
+};
+
+console.log(player);
+console.log(player.name);
+player.points = player.points + 15;
+console.log(player.points);
