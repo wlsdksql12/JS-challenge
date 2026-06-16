e2dcbad34124b0c133a4bb82aa4357f518181a97
wlsdksql12 <90670621+wlsdksql12@users.noreply.github.com>
2026-06-16 17:41:20 +0900

    함수의 개념 및 파라미터 활용법 복습

--- app.js
+++ app.js
@@ -19,13 +19,48 @@ const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
 console.log(daysOfWeek);
 
+// const player = {
+//   name: "asj",
+//   points: 10,
+//   fat: true,
+// };
+
+// console.log(player);
+// console.log(player.name);
+// player.points = player.points + 15;
+// console.log(player.points);
+
+// function sayHello() {
+//   console.log("Hello my name is " + player.name);
+// }
+
+// sayHello();
+
+// function sayHello(nameOfPerson, age) {
+//   console.log("Hello my name is " + nameOfPerson + "and I'm " + age);
+// }
+
+// sayHello("nico", 10);
+// sayHello("dal", 23);
+// sayHello("lynn", 21);
+
+function plus(a, b) {
+  console.log(a + b);
+}
+
+function divide(a, b) {
+  console.log(a + b);
+}
+
+plus(8, 60);
+divide(98 / 20);
+
 const player = {
   name: "asj",
-  points: 10,
-  fat: true,
+  seyHello: function (otherPersonsName) {
+    console.log("Hello " + otherPersonsName + " nice to meet you  ");
+  },
 };
 
-console.log(player);
 console.log(player.name);
-player.points = player.points + 15;
-console.log(player.points);
+player.seyHello("lynn");
