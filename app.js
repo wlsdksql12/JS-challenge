d6b6299af342188870698eecd29d3273382eca2e
wlsdksql12 <90670621+wlsdksql12@users.noreply.github.com>
2026-06-19 00:03:15 +0900

    함수의 return 개념 및 조건문 복습

--- app.js
+++ app.js
@@ -64,3 +64,30 @@ const player = {
 
 console.log(player.name);
 player.seyHello("lynn");
+
+// const age = 96;
+
+// function calculateKrAge(ageOfForeigner) {
+//   return ageOfForeigner + 2;
+// }
+
+// const krAge = calculateKrAge(age);
+
+// console.log(krAge);
+
+const age = parseInt(prompt("How old are you?"));
+
+console.log(age);
+console.log(isNaN(age));
+
+if (isNaN(age) || age < 0) {
+  console.log("Please write a real positive number");
+} else if (age < 18) {
+  console.log("You are too young");
+} else if (age >= 18 && age <= 50) {
+  console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+  console.log("You should exercise");
+} else if (age > 80) {
+  console.log("You can do whatever you want.");
+}
