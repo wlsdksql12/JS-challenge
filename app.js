69452b72da38fbf9772af2b7c36d31f2137b2675
wlsdksql12 <90670621+wlsdksql12@users.noreply.github.com>
2026-06-19 18:12:43 +0900

    querySelector, addEventListener 활용 복습

--- app.js
+++ app.js
@@ -75,19 +75,68 @@ player.seyHello("lynn");
 
 // console.log(krAge);
 
-const age = parseInt(prompt("How old are you?"));
+// const age = parseInt(prompt("How old are you?"));
 
-console.log(age);
-console.log(isNaN(age));
+// console.log(age);
+// console.log(isNaN(age));
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a real positive number");
-} else if (age < 18) {
-  console.log("You are too young");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise");
-} else if (age > 80) {
-  console.log("You can do whatever you want.");
+// if (isNaN(age) || age < 0) {
+//   console.log("Please write a real positive number");
+// } else if (age < 18) {
+//   console.log("You are too young");
+// } else if (age >= 18 && age <= 50) {
+//   console.log("You can drink");
+// } else if (age > 50 && age <= 80) {
+//   console.log("You should exercise");
+// } else if (age > 80) {
+//   console.log("You can do whatever you want.");
+// }
+
+// const title = document.getElementById("title");
+
+// title.innerText = "Got you!";
+
+// console.log(title.id);
+// console.log(title.className);
+
+const title = document.querySelector(".hello h1");
+
+title.innerText = "Hello";
+
+function handleTitleClick() {
+  title.style.color = "blue";
 }
+
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOOD");
+}
+
+//title.addEventListener("click", handleTitleClick);
+title.onclick = handleTitleClick;
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
